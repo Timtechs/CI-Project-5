0221f1af0ecb41537a783a27d155725bcd5e5a63
Timtechs <Timmy.s.deckert@gmail.com>
2022-12-30 15:19:24 +0000

    Try again

--- titan/settings.py
+++ titan/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'titan-timtech'
+    AWS_STORAGE_BUCKET_NAME = 'Manage-titan'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
